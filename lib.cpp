da6ebe29a2d6b811c6a0303fd40dc2441db73e30
Nicola-besana <115076634+Nicola-besana@users.noreply.github.com>
2022-12-08 22:44:01 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,3 +1,13 @@
 
+#include"lib.h"
+bool input (char &let){
+    if(let>=65 && let<=90) {
+        let = let + 32;
+        return true;
+    }else if(let>=97 && let<=122){
+        let=let-32;
+        return true;
+    }else{
+        return false;
+    }}
 
-bool input (char &let);
