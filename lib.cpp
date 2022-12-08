7ea44612914664658bc22b0188577e4648293bcc
Nicola-besana <115076634+Nicola-besana@users.noreply.github.com>
2022-12-08 22:47:49 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,13 +1,22 @@
 
-#include"lib.h"
-bool input (char &let){
-    if(let>=65 && let<=90) {
-        let = let + 32;
-        return true;
-    }else if(let>=97 && let<=122){
-        let=let-32;
-        return true;
-    }else{
-        return false;
-    }}
+#include "lib.h"
 
+bool letteraono(int valore){
+    if (valore>=65 && valore<=90 || valore>=97 && valore<=122){
+        return true;
+    }
+    else
+        return false;
+}
+
+char conversionelettere(char valore){
+     
+        char esito;
+        if( valore>='A' && valore<='Z'){
+         esito=valore+32;
+        }
+        else if(valore>='a' && valore<='z'){
+         esito=valore-32;
+        }
+        return esito;
+}
